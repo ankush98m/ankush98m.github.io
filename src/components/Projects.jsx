4d5703c00d28af9681a344c4af23f563f69fee71
Ankush Maheshwari <ankush97m@gmail.com>
2024-05-31 14:22:51 -0400

    added skills in projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Divider, Container, Typography, Button } from '@mui/material';
+import { Divider, Container, Typography, Button, Stack, Chip } from '@mui/material';
 import spendCubes from "../assets/images/spendCubes.PNG";
 import CodonBiotech from "../assets/images/codonBiotech.PNG";
 import ARApp from "../assets/images/ARApp.png";
@@ -14,7 +14,7 @@ function Projects() {
             title: 'CheckMate Challenge',
             descrption: "Checkmate Challenge is a variation of the traditional chess. The Checkmate Challenge is an Android app offering bite-sized chess puzzles. Players must checkmate the king in a limited number of moves on compact boards with varying piece arrangements. With each level presenting a new challenge, it's a stimulating and strategic game for chess enthusiasts on the go.",
             image: Chess,
-            technology: [],
+            technology: ['Java', 'OOPS Concepts', 'MiniMax Algorithm', 'Android Studio'],
             github: 'https://github.com/maheshwari-ank/CheckmateChallenge',
             link: ''
         },
@@ -22,7 +22,7 @@ function Projects() {
             title: 'IPL Data Analysis',
             descrption: 'Analyzed IPL cricket data using Bayesian simulation and Markov Chain Monte Carlo to identify factors affecting team winning probabilities and to evaluate bowler performance in IPL matches, recommending optimal bowlers for taking wickets based on historical data and match situations',
             image: IPL,
-            technology: [],
+            technology: ['Python', 'Bayesian Simulation', 'MCMC Algorithm', 'Jupyter Notebook'],
             github: 'https://github.com/ankush98m/IPL-data-analysis',
             link: ''
         },
@@ -30,7 +30,7 @@ function Projects() {
             title: 'Harmony Hub',
             descrption: ' HarmonyHub redefines the way we experience and share music by introducing a novel approach to collaborative playlist curation. Unlike traditional music platforms, HarmonyHub focuses on real-time interaction, allowing users to collaboratively build and curate playlists together, transforming the act of music discovery into a social, dynamic, and engaging experience.',
             image: harmonyHub,
-            technology: [],
+            technology: ['Typescript', 'React', 'Node.js', 'Express', 'MongoDB', 'HTML', 'CSS', 'SCSS'],
             github: 'https://github.com/ankush98m/harmonyHub',
             link: 'https://youtu.be/XFxfRkLyJnM'
         },
@@ -38,7 +38,7 @@ function Projects() {
             title: 'Spend Cubes',
             descrption: 'A responsive website for a startup that manages and helps to audit their telecommunication and wifi bills. Professionals at Spend Cubes bring together decades of experience and expertise in telecom lifecycle management, refunds and recovery.',
             image: spendCubes,
-            technology: [],
+            technology: ['Javascript', 'React', 'HTML5', 'CSS3'],
             github: 'https://github.com/ankush98m/Spend_Cubes',
             link: 'https://spendcubes.com/'
         },
@@ -46,7 +46,7 @@ function Projects() {
             title: 'Codon Biotech',
             descrption: 'Responsive Landing Page for Codon Biotech. Codon Biotech Pvt. Ltd. (CBPL) is a company incorporated v/s section 75 of Companies Act, 1956. It was established in 2007 with an aim to encourage Biotechnology related research, product development and Skill development which would lead to bridging the gap between Academics & Industry.',
             image: CodonBiotech,
-            technology: [],
+            technology: ['Javascript', 'React', 'HTML5', 'CSS3'],
             github: 'https://github.com/ankush98m/Spend_Cubes',
             link: 'https://codonbiotech.vercel.app/'
         },
@@ -154,7 +154,18 @@ function Projects() {
                                                 <img src={p.image} alt={p.title} className='projImg-left' sx={{objectFit:"contain"}}/>
                                             </div>
                                             <div style={{ flexDirection: 'column' }}>
-                                                <Typography variant='body1' className='projectDesc-left'>{p.descrption}</Typography>
+                                                <Typography variant='body1' className='projectDesc-left'>
+                                                    {p.descrption}
+                                                </Typography>
+                                                <Stack direction="row" spacing={1} className='skill-stack-left'>
+                                                {p.technology.map((t,i)=>{
+                                                    return(
+                                                        
+                                                            <Chip label={t} color= "primary" sx={{backgroundColor:"#8884fc"}} />
+                                                        
+                                                    )
+                                                })}
+                                                </Stack>
                                                 <div className='leftProjBtns'>
                                                     {/* <Button variant="contained" className="projBtn-left" href={p.link} target="_blank">
                                                         Open Project
@@ -173,7 +184,17 @@ function Projects() {
                                             <div className='projectCard-right'>
                                                 <div style={{ flexDirection: 'column' }}>
 
-                                                    <Typography variant='body1' className='projectDesc-right'>{p.descrption}</Typography>
+                                                    <Typography variant='body1' className='projectDesc-right'>
+                                                        {p.descrption}</Typography>
+                                                        <Stack direction="row" spacing={1} className='skill-stack-right'>
+                                                            {p.technology.map((t,i)=>{
+                                                                return(
+                                                                    
+                                                                        <Chip label={t} color= "primary" sx={{backgroundColor:"#8884fc"}}/>
+                                                                    
+                                                                )
+                                                            })}
+                                                        </Stack>
                                                     <div className='rightProjBtns'>
                                                         {/* <Button variant="contained" className="projBtn-right" href={p.link} target="_blank"> */}
                                                             {/* Open Project */}
@@ -204,6 +225,13 @@ function Projects() {
                                                 <div style={{ flexDirection: 'column' }}>
                                                     
                                                     <Typography variant='body1' className='projectDesc-right'>{p.descrption}</Typography>
+                                                    <Stack direction="row" spacing={1} className='skill-stack-right'>
+                                                            {p.technology.map((t,i)=>{
+                                                                return(
+                                                                    <Chip label={t} color= "primary" sx={{backgroundColor:"#8884fc"}}/>
+                                                                )
+                                                            })}
+                                                        </Stack>
                                                     <div className='rightProjBtns'>
                                                         {/* <Button variant="contained" className="projBtn-right" href={p.link} target="_blank">
                                                             Open Project
