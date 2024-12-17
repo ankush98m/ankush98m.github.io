fd6ac8e4bd8ac68de5cc135cbf4dc5094dd30c91
Ankush Maheshwari <ankush97m@gmail.com>
2024-12-16 17:21:41 -0800

    add svg's for skills

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,122 +1,172 @@
-import React, {useEffect} from 'react';
-import Navbar from './Navbar';
-import { Button, Typography } from '@mui/material';
-import Grid from '@mui/material/Grid';
+import React, { useEffect } from "react";
+import Navbar from "./Navbar";
+import { Button, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
 // import Collage from "../assets/images/collage1.jpg"
-import photo1 from "../assets/images/collagePhoto1.jpeg"
-import photo2 from "../assets/images/collagePhoto2.jpeg"
-import photo3 from "../assets/images/aboutPhoto.png"
+// import photo1 from "../assets/images/collagePhoto1.jpeg";
+// import photo2 from "../assets/images/collagePhoto2.jpeg";
+// import photo3 from "../assets/images/aboutPhoto.png";
 // import photo3 from "../assets/images/collagePhoto4.jpeg"
-import Cplusplus from "../assets/images/c++_icon.png";
+import Python from "../assets/images/python.svg";
 import js from "../assets/images/javascript.svg";
-import react from "../assets/images/react.png";
+import Java from "../assets/images/Java.svg"
+// import Postgres from "../assets/images/Postgresql.svg"
+import reactNative from "../assets/images/react-native.png"
+import react from "../assets/images/react.svg";
 import redux from "../assets/images/redux.svg";
-import mongo from "../assets/images/mongodb.png"
+import mongo from "../assets/images/mongodb.svg";
 import express from "../assets/images/express.png";
-import node from "../assets/images/node.png";
-import hclogo from "../assets/images/htmlcss.jpg";
-import Footer from './Footer';
-import { red } from '@mui/material/colors';
+import node from "../assets/images/nodejs.svg";
+import aws from "../assets/images/aws.svg"
+import html5 from "../assets/images/html5.svg"
+import css3 from "../assets/images/css-3.svg"
+import Footer from "./Footer";
+import { red } from "@mui/material/colors";
 
 function About() {
-    useEffect(() => {
-            // const elements = document.getElementsByClassName('expCard');
-            const heading = document.getElementsByClassName('aboutHeading');
-    
-            const observer = new IntersectionObserver(entries => {
-                entries.forEach(entry => {
-                    if (entry.isIntersecting) {
-                        entry.target.classList.add('expAnimation');
-                    } else {
-                        entry.target.classList.remove('expAnimation');
-                    }
-                });
-            });
-    
-            const observer2 = new IntersectionObserver(entries => {
-                entries.forEach(entry => {
-                    if (entry.isIntersecting) {
-                        entry.target.classList.add('headAnimation');
-                    } else {
-                        entry.target.classList.remove('headAnimation');
-                    }
-                });
-            });
-    
-            // Loop through elements and observe each one
-            // Array.from(elements).forEach(element => {
-            //     observer.observe(element);
-            // });
-    
-            Array.from(heading).forEach(element => {
-                observer2.observe(element);
-            });
-    
-            // Cleanup observer when component unmounts
-            return () => {
-                // Array.from(elements).forEach(element => {
-                //     observer.unobserve(element);
-                // });
-    
-                Array.from(heading).forEach(element => {
-                    observer2.unobserve(element);
-                });
-            };
-        }, []);
-    return (
-        <div id="about">
-            <div className='aboutGradient'>
-                <Typography variant='h2' className='aboutHeading'>About Me</Typography>
-                <div className='aboutDiv'>
-                    <div className='aboutLeft'>
-                        {/* <img src={photo1} alt="Ankush Maheshwari" />
+  useEffect(() => {
+    // const elements = document.getElementsByClassName('expCard');
+    const heading = document.getElementsByClassName("aboutHeading");
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("expAnimation");
+        } else {
+          entry.target.classList.remove("expAnimation");
+        }
+      });
+    });
+
+    const observer2 = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("headAnimation");
+        } else {
+          entry.target.classList.remove("headAnimation");
+        }
+      });
+    });
+
+    // Loop through elements and observe each one
+    // Array.from(elements).forEach(element => {
+    //     observer.observe(element);
+    // });
+
+    Array.from(heading).forEach((element) => {
+      observer2.observe(element);
+    });
+
+    // Cleanup observer when component unmounts
+    return () => {
+      // Array.from(elements).forEach(element => {
+      //     observer.unobserve(element);
+      // });
+
+      Array.from(heading).forEach((element) => {
+        observer2.unobserve(element);
+      });
+    };
+  }, []);
+  return (
+    <div id="about">
+      <div className="aboutGradient">
+        <Typography variant="h2" className="aboutHeading">
+          About Me
+        </Typography>
+        <div className="aboutDiv">
+          <div className="aboutLeft">
+            {/* <img src={photo1} alt="Ankush Maheshwari" />
                         <img src={photo2} alt="Ankush Maheshwari" /> */}
-                        {/* <img src={photo3} height="auto" width="200px" /> */}
-                    </div>
-                    <div className='aboutRight'>
-                        <Typography sx={{
-                            color: "#c3c0c0",
-                            textAlign: 'left'
-                        }}>Currently pursuing my master's at Northeastern University, I bring nearly three years of industry experience in web development to the table. I'm driven by a constant quest for knowledge, diving into intriguing technologies to further evolve my skill set. Beyond my professional endeavors, I find joy in exploring new destinations, diving into captivating video games, and prioritizing my fitness regimen during my leisure time</Typography>
-                        <Typography sx={{
-                            color: "#c3c0c0",
-                            padding: '3% 0%'
-                        }}>Here are some technical skills I am proficient at</Typography>
-                        <Grid container rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3}}>
-                            <Grid item xs={4} sm={4} md={3} lg={3} xl={3}>
-                                <img src={Cplusplus} alt="C++" className='skill-img'/>
-                            </Grid>
-                            <Grid item xs={4} sm={4} md={3} lg={3} xl={3}>
-                            <img src={js} alt="Javascript" className='skill-img'/>
-                            </Grid>
-                            <Grid item xs={4} sm={4} md={3} lg={3} xl={3}>
-                            <img src={react} alt="Reactjs" className='skill-img'/>
-                            </Grid>
-                            <Grid item xs={4} sm={4} md={3} lg={3} xl={3}>
-                            <img src={redux} alt="Redux" className='skill-img'/>
-                            </Grid>
-                            <Grid item xs={4} sm={4} md={3} lg={3} xl={3}>
-                            <img src={mongo} alt="MongoDB" className='skill-img'/>
-                            </Grid>
-                            <Grid item xs={4} sm={4} md={3} lg={3} xl={3}>
-                            <img src={express} alt="Expressjs" className='skill-img'/>
-                            </Grid>
-                            <Grid item xs={4} sm={4} md={3} lg={3} xl={3}>
-                            <img src={node} alt="Nodejs" className='skill-img'/>
-                            </Grid>
-                            <Grid item xs={4} sm={4} md={3} lg={3} xl={3}>
-                            <img src={hclogo} alt="HTML/CSS" className='skill-img'/>
-                            </Grid>
-                        </Grid>
+            {/* <img src={photo3} height="auto" width="200px" /> */}
+          </div>
+          <div className="aboutRight">
+            <Typography
+              sx={{
+                color: "#c3c0c0",
+                textAlign: "left",
+              }}
+            >
+              I am currently pursuing my master's degree at Northeastern
+              University, I bring nearly three years of industry experience in
+              web development to the table. I'm driven by a constant quest for
+              knowledge, diving into intriguing technologies to further evolve
+              my skill set.
+              <br />
+              <br />I am currently working on a family healthcare project to
+              digitalize health and provide the care your family needs. This
+              mobile application aims to achieve secure and easy access to your
+              healthcare records, summarize your reports, and view medical
+              history in one place. This app is integrated with AI chatbot so
+              that you can solve your health related queries without any hassle.
+              Also, monitor your health vitals like heart rate, blood pressure
+              and sugar level using our data visualizations.
+              <br />
+              <br />
+              Beyond my professional endeavors, I find joy in exploring new
+              destinations, diving into captivating video games, and
+              prioritizing my fitness regimen during my leisure time.
+            </Typography>
+            <Typography
+              sx={{
+                color: "#c3c0c0",
+                padding: "3% 0%",
+              }}
+            >
+              Here are some technical skills I am proficient at
+            </Typography>
 
-                    </div>
-                </div>
-                <div className='aboutEmptyDiv'>
-
-                </div>
-            </div>
+            <Grid
+              container
+              rowSpacing={3}
+              columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+            >
+              <Grid item xs={4} sm={4} md={3} lg={3} xl={3}>
+                <img src={js} alt="Javascript" className="skill-img" />
+              </Grid>
+              <Grid item xs={4} sm={4} md={3} lg={3} xl={3}>
+                <img src={Python} alt="Python" className="skill-img" />
+              </Grid>
+              <Grid item xs={4} sm={4} md={3} lg={3} xl={3}>
+                <img src={react} alt="Reactjs" className="skill-img" />
+              </Grid>
+              <Grid item xs={4} sm={4} md={3} lg={3} xl={3}>
+                <img src={reactNative} alt="React-Native" className="skill-img" />
+              </Grid>
+              <Grid item xs={4} sm={4} md={3} lg={3} xl={3}>
+                <img src={redux} alt="Redux" className="skill-img" />
+              </Grid>
+              <Grid item xs={4} sm={4} md={3} lg={3} xl={3}>
+                <img src={Java} alt="Java" className="skill-img" />
+              </Grid>
+              <Grid item xs={4} sm={4} md={3} lg={3} xl={3}>
+                <img src={mongo} alt="MongoDB" className="skill-img" />
+              </Grid>
+              {/* <Grid item xs={4} sm={4} md={3} lg={3} xl={3}>
+                <img src={Postgres} alt="Postgres" className="skill-img" />
+              </Grid> */}
+              <Grid item xs={4} sm={4} md={3} lg={3} xl={3}>
+                <img src={express} alt="Expressjs" className="skill-img" />
+              </Grid>
+              <Grid item xs={4} sm={4} md={3} lg={3} xl={3}>
+                <img src={node} alt="Nodejs" className="skill-img" />
+              </Grid>
+              <Grid item xs={4} sm={4} md={3} lg={3} xl={3}>
+                <img src={aws} alt="AWS" className="skill-img" />
+              </Grid>
+              <Grid item xs={4} sm={4} md={3} lg={3} xl={3}>
+                <img src={html5} alt="HTML" className="skill-img" />
+              </Grid>
+              <Grid item xs={4} sm={4} md={3} lg={3} xl={3}>
+                <img src={css3} alt="HTML" className="skill-img" />
+              </Grid>
+            </Grid>
+          </div>
         </div>
-    )
+        <div className="aboutEmptyDiv"></div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,92 +1,146 @@
-import React from 'react';
+import React from "react";
 // import CoverImage from "../assets/images/coverPhoto2.jpeg"
 
-import { Typography } from '@mui/material';
+import { Typography } from "@mui/material";
 // import List from '@mui/material/List';
 // import ListItemText from '@mui/material/ListItemText';
-import { useScroll, motion} from "framer-motion";
-import CircleIcon from '@mui/icons-material/Circle';
-import About from "./About"
-import Navbar from './Navbar';
-import Projects from './Projects';
-import Experiences from './Experiences';
-import Footer from './Footer';
+import { useScroll, motion } from "framer-motion";
+import CircleIcon from "@mui/icons-material/Circle";
+import About from "./About";
+import Navbar from "./Navbar";
+import Projects from "./Projects";
+import Experiences from "./Experiences";
+import Footer from "./Footer";
+import ankushPhoto from "../assets/images/ankush_photo.jpg";
 
 function Home() {
-    const { scrollYProgress } = useScroll();
+  const { scrollYProgress } = useScroll();
 
-    return (
-        <div style={{overflowX: 'hidden'}}>
-            <motion.div
-                className="progress-bar"
-                style={{ scaleX: scrollYProgress }}
-            />
+  return (
+    <div style={{ overflowX: "hidden" }}>
+      <motion.div
+        className="progress-bar"
+        style={{ scaleX: scrollYProgress }}
+      />
 
-            <div className='container-fluid'>
+      <div className="container-fluid">
+        <div className="coverGradient">
+          <Navbar />
 
-                <div className='coverGradient' >
-                    <Navbar />
-                    <Typography variant='h3' className='coverHeading' 
-                    style={{
-                        position: 'relative', 
-                        color: '#1b1b1b'
-                    }}>Hi, I am <span id="headingSpan" style={{
-                        color: 'snow'
-                    }}>Ankush Maheshwari</span></Typography>
-                    <div className='coverList'>
-                        <span id="webDiv" style={{ display: 'flex'}}>
-                            <CircleIcon className="circle-icon" sx={{ position: 'relative' }} />
-                            <div id="webList">
-                                <span style={{'--i': 1}}>W</span>
-                                <span style={{'--i': 2}}>E</span>
-                                <span style={{'--i': 3}}>B</span>
-                                <span>&nbsp;</span>
-                                {/* <span style={{'--i': 4}}> </span> */}
-                                <span style={{'--i': 4}}>D</span>
-                                <span style={{'--i': 5}}>E</span>
-                                <span style={{'--i': 6}}>V</span>
-                                <span style={{'--i': 7}}>E</span>
-                                <span style={{'--i': 8}}>L</span>
-                                <span style={{'--i': 9}}>O</span>
-                                <span style={{'--i': 10}}>P</span>
-                                <span style={{'--i': 11}}>E</span>
-                                <span style={{'--i': 12}}>R</span>
-                            </div>
-                        </span>
-                        <span id="softwareDiv" style={{ display: 'flex'}}>
-                            <CircleIcon className="circle-icon" sx={{ position: 'relative' }} />
-                            <div id="softwareList">
-                                <span style={{'--i': 1}}>S</span>
-                                <span style={{'--i': 2}}>O</span>
-                                <span style={{'--i': 3}}>F</span>
-                                <span style={{'--i': 4}}>T</span>
-                                <span style={{'--i': 5}}>W</span>
-                                <span style={{'--i': 6}}>A</span>
-                                <span style={{'--i': 7}}>R</span>
-                                <span style={{'--i': 8}}>E</span>
-                                <span>&nbsp;</span>
-                                <span style={{'--i': 9}}>D</span>
-                                <span style={{'--i': 10}}>E</span>
-                                <span style={{'--i': 11}}>V</span>
-                                <span style={{'--i': 12}}>E</span>
-                                <span style={{'--i': 13}}>L</span>
-                                <span style={{'--i': 14}}>O</span>
-                                <span style={{'--i': 15}}>P</span>
-                                <span style={{'--i': 16}}>E</span>
-                                <span style={{'--i': 17}}>R</span>
-                            </div>
-                        </span>
-                    </div>
-
-
-                </div>
-            </div>
-            <About />
-            <Projects />
-            <Experiences />
-            <Footer />
+          <Typography
+            variant="h3"
+            className="coverHeading"
+            style={{
+              position: "relative",
+              color: "#1b1b1b",
+            }}
+          >
+            Hi, I am{" "}
+            <span
+              id="headingSpan"
+              style={{
+                color: "snow",
+              }}
+            >
+              Ankush Maheshwari
+            </span>
+          </Typography>
+          <div className="coverList">
+            {/* <img src={ankushPhoto} alt="" className="coverPhoto" width="200px" /> */}
+            <span id="webDiv" style={{ display: "flex" }}>
+              <CircleIcon
+                className="circle-icon"
+                sx={{ position: "relative" }}
+              />
+              <div id="webList">
+                <span style={{ "--i": 1 }}>W</span>
+                <span style={{ "--i": 2 }}>E</span>
+                <span style={{ "--i": 3 }}>B</span>
+                <span>&nbsp;</span>
+                <span style={{ "--i": 4 }}>/</span>
+                <span>&nbsp;</span>
+                <span style={{ "--i": 5 }}>A</span>
+                <span style={{ "--i": 6 }}>P</span>
+                <span style={{ "--i": 7 }}>P</span>
+                <span>&nbsp;</span>
+                <span style={{ "--i": 8 }}>D</span>
+                <span style={{ "--i": 9 }}>E</span>
+                <span style={{ "--i": 10 }}>V</span>
+                <span style={{ "--i": 11 }}>E</span>
+                <span style={{ "--i": 12 }}>L</span>
+                <span style={{ "--i": 13 }}>O</span>
+                <span style={{ "--i": 14 }}>P</span>
+                <span style={{ "--i": 15 }}>E</span>
+                <span style={{ "--i": 16 }}>R</span>
+              </div>
+            </span>
+            <span id="softwareDiv" style={{ display: "flex" }}>
+              <CircleIcon
+                className="circle-icon"
+                sx={{ position: "relative" }}
+              />
+              <div id="softwareList">
+                <span style={{ "--i": 1 }}>S</span>
+                <span style={{ "--i": 2 }}>O</span>
+                <span style={{ "--i": 3 }}>F</span>
+                <span style={{ "--i": 4 }}>T</span>
+                <span style={{ "--i": 5 }}>W</span>
+                <span style={{ "--i": 6 }}>A</span>
+                <span style={{ "--i": 7 }}>R</span>
+                <span style={{ "--i": 8 }}>E</span>
+                <span>&nbsp;</span>
+                <span style={{ "--i": 9 }}>D</span>
+                <span style={{ "--i": 10 }}>E</span>
+                <span style={{ "--i": 11 }}>V</span>
+                <span style={{ "--i": 12 }}>E</span>
+                <span style={{ "--i": 13 }}>L</span>
+                <span style={{ "--i": 14 }}>O</span>
+                <span style={{ "--i": 15 }}>P</span>
+                <span style={{ "--i": 16 }}>E</span>
+                <span style={{ "--i": 17 }}>R</span>
+              </div>
+            </span>
+            <span id="learningDiv" style={{ display: "flex" }}>
+              <CircleIcon
+                className="circle-icon"
+                sx={{ position: "relative" }}
+              />
+              <div id="learningList">
+                <span style={{ "--i": 1 }}>D</span>
+                <span style={{ "--i": 2 }}>E</span>
+                <span style={{ "--i": 3 }}>E</span>
+                <span style={{ "--i": 4 }}>P</span>
+                <span>&nbsp;</span>
+                <span style={{ "--i": 5 }}>L</span>
+                <span style={{ "--i": 6 }}>E</span>
+                <span style={{ "--i": 7 }}>A</span>
+                <span style={{ "--i": 8 }}>R</span>
+                <span style={{ "--i": 9 }}>N</span>
+                <span style={{ "--i": 10 }}>I</span>
+                <span style={{ "--i": 11 }}>N</span>
+                <span style={{ "--i": 12 }}>G</span>
+                <span>&nbsp;</span>
+                <span style={{ "--i": 13 }}>E</span>
+                <span style={{ "--i": 14 }}>N</span>
+                <span style={{ "--i": 15 }}>T</span>
+                <span style={{ "--i": 16 }}>H</span>
+                <span style={{ "--i": 17 }}>U</span>
+                <span style={{ "--i": 18 }}>S</span>
+                <span style={{ "--i": 19 }}>I</span>
+                <span style={{ "--i": 20 }}>A</span>
+                <span style={{ "--i": 21 }}>S</span>
+                <span style={{ "--i": 22 }}>T</span>
+              </div>
+            </span>
+          </div>
         </div>
-    )
+      </div>
+      <About />
+      <Projects />
+      <Experiences />
+      <Footer />
+    </div>
+  );
 }
 
 export default Home;
